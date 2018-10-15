e6fdb478e050aca94468f7b59300cec6bbfea906
Ryan <bourhilr@gmail.com>
2018-10-15 22:08:40 +0200

    add timeAgo filter

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -3,7 +3,7 @@
 
     <input type='checkbox' v-model="todo.checked">
      {{this.todo.text}}
-     <span class='timeago'> - 5 minutes ago</span>
+     <span class='timeago'> {{ todo.createdAt | timeAgo }} </span>
   </div>
 </template>
 
@@ -13,6 +13,26 @@ export default {
   props: {
     todo: {
       required: true
+    }
+  },
+  filters: {
+    timeAgo (date) {
+      const seconds = Math.floor((new Date() - date) / 1000)
+      const formats = [
+        { format: 'year', duration: 356 * 24 * 60 * 60 },
+        { format: 'month', duration: 30 * 24 * 60 * 60 },
+        { format: 'week', duration: 7 * 24 * 60 * 60 },
+        { format: 'day', duration: 24 * 60 * 60 },
+        { format: 'hour', duration: 60 * 60 },
+        { format: 'minute', duration: 60 },
+        { format: 'second', duration: 1 }
+      ]
+      for (const f of formats) {
+        const interval = Math.floor(seconds / f.duration)
+        const format = interval > 1 ? f.format + 's' : f.format
+        if (interval >= 1) return `${interval} ${format} ago`
+      }
+      return 'Just now'
     }
   }
 }
