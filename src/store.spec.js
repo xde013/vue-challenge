f80bf5fd019fc92aa4ef20890f20055977c7b4a4
Ryan <bourhilr@gmail.com>
2018-10-15 23:16:11 +0200

    add removeTodo test

--- src/store.spec.js
+++ src/store.spec.js
@@ -10,17 +10,36 @@ beforeEach(() => {
 
 describe('TodoStore', () => {
   it('addTodo', () => {
+    // Arrange
     const testTodo = {
       text: 'addTodoTest',
       checked: true,
       createdAt: new Date()
     }
+    // Act
     mutations.addTodo(initialState, testTodo)
-
+    // Assert
     expect(initialState).toEqual({
       todos: [
         testTodo
       ]
     })
+  })
+  it('deleteTodo', () => {
+    // Arrange
+    const testTodo = {
+      text: 'addTodoTest',
+      checked: true,
+      createdAt: new Date()
+    }
+    initialState.todos.push(testTodo)
+
+    // Act
+    mutations.removeTodo(initialState, testTodo)
+
+    // Assert
+    expect(initialState.todos).toEqual(
+      expect.not.arrayContaining([testTodo])
+    )
   })
 })
